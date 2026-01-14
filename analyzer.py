1a1ff953e50cdc16acd3d6f249e6a33a457397a1
MosheKash <kashlinskymoshe@gmail.com>
2026-01-13 22:48:41 -0500

    Added job scraping functionality, and the ability to input custom jobs. Coming along very nicely. All that is left is to figure out the actual meat of the project, the analyzer and resume creator. Maybe want to also implement a feature where it rates how good of a fit you are for the job description.

--- analyzer.py
+++ analyzer.py
@@ -3,6 +3,11 @@ import csv
 from datetime import datetime
 import shutil
 import pandas as pd
+from jobspy import scrape_jobs
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from datetime import date
+
 # General Overview:
 
 # 1. Ask for a job description, ideally a LinkedIn/Handshake/Indeed link that I then scrape for data
@@ -30,7 +35,7 @@ def adjust_user_info():
     print("1. Skills")
     print("2. Coursework")
     print("3. Projects")
-    print("4. Work Experience\n")
+    print("4. Work Experience")
     print("5. Return to Main Menu\n")
     while True:
         result = input("Selection: ")
@@ -39,19 +44,19 @@ def adjust_user_info():
         print("Invalid input. Please type 1 to adjust skills, 2 to adjust coursework, 3 to adjust projects, 4 to adjust work experience, or 5 to return to the main menu")
 
 def adjust_skills():
-    valid = {'1': view_skills, '2': add_skill, '3': remove_skill, '4': edit_skill}
+    valid = {'1': view_skills, '2': add_skill, '3': remove_skill, '4': edit_skill, '5': adjust_user_info}
     print("Would you like to add, remove, view, or edit a skill?\n")
     print("1. View Skills")
     print("2. Add Skill")
     print("3. Remove Skill")
-    print("4. Edit Skill\n")
-
+    print("4. Edit Skill")
+    print("5. Return to User Info\n")
     while True:
         result = input("Selection: ")
         if result in valid:
             return valid[result]()
         else:
-            print("Invalid input. Please type 1 to view skills, 2 to add a skill, 3 to remove a skill, or 4 to edit a skill")
+            print("Invalid input. Please type 1 to view skills, 2 to add a skill, 3 to remove a skill, 4 to edit a skill, or 5 to return to user info")
 
 def view_skills():
     skills_df = pd.read_csv('Stored Info/skills_bank.csv')
@@ -125,19 +130,20 @@ def edit_skill():
             print(f"Skill '{skill_to_edit}' not found.")
 
 def adjust_coursework():
-    valid = {'1': view_coursework, '2': add_coursework, '3': remove_coursework, '4': edit_coursework}
+    valid = {'1': view_coursework, '2': add_coursework, '3': remove_coursework, '4': edit_coursework, '5': adjust_user_info}
     print("Would you like to add, remove, view, or edit a skill?\n")
     print("1. View Coursework")
     print("2. Add Coursework")
     print("3. Remove Coursework")
-    print("4. Edit Coursework\n")
-
+    print("4. Edit Coursework")
+    print("5. Return to User Info\n")
+    
     while True:
         result = input("Selection: ")
         if result in valid:
             return valid[result]()
         else:
-            print("Invalid input. Please type 1 to view coursework, 2 to add coursework, 3 to remove coursework, or 4 to edit coursework")
+            print("Invalid input. Please type 1 to view coursework, 2 to add coursework, 3 to remove coursework, 4 to edit coursework, or 5 to return to user info")
 
 def view_coursework():
     coursework_df = pd.read_csv('Stored Info/coursework_bank.csv')
@@ -240,18 +246,20 @@ def edit_coursework():
             print(f"Course with ID '{course_to_edit}' not found.")
 
 def adjust_projects():
-    valid = {'1': view_projects, '2': add_project, '3': remove_project, '4': edit_project}
+    valid = {'1': view_projects, '2': add_project, '3': remove_project, '4': edit_project, '5': adjust_user_info}
     print("Would you like to add, remove, view, or edit a project?\n")
     print("1. View Projects")
     print("2. Add Project")
     print("3. Remove Project")
-    print("4. Edit Project\n")
+    print("4. Edit Project")
+    print("5. Return to User Info\n")
+    
     while True:
         result = input("Selection: ")
         if result in valid:
             return valid[result]()
         else:
-            print("Invalid input. Please type 1 to view projects, 2 to add a project, 3 to remove a project, or 4 to edit a project")
+            print("Invalid input. Please type 1 to view projects, 2 to add a project, 3 to remove a project, 4 to edit a project, or 5 to return to user info")
 
 def view_projects():
     projects_df = pd.read_csv('Stored Info/projects_bank.csv')
@@ -346,18 +354,20 @@ def edit_project():
             print(f"Project '{project_to_edit}' not found.")
 
 def adjust_work_experience():
-    valid = {'1': view_work_experience, '2': add_work_experience, '3': remove_work_experience, '4': edit_work_experience}
+    valid = {'1': view_work_experience, '2': add_work_experience, '3': remove_work_experience, '4': edit_work_experience, '5': adjust_user_info}
     print("Would you like to add, remove, view, or edit a work experience?\n")
     print("1. View Work Experience")
     print("2. Add Work Experience")
     print("3. Remove Work Experience")
-    print("4. Edit Work Experience\n")
+    print("4. Edit Work Experience")
+    print("5. Return to User Info\n")
+    
     while True:
         result = input("Selection: ")
         if result in valid:
             return valid[result]()
         else:
-            print("Invalid input. Please type 1 to view work experience, 2 to add work experience, 3 to remove work experience, or 4 to edit work experience")
+            print("Invalid input. Please type 1 to view work experience, 2 to add work experience, 3 to remove work experience, 4 to edit work experience, or 5 to return to user info")
 
 def view_work_experience():
     work_experience_df = pd.read_csv('Stored Info/work_experience_bank.csv')
@@ -463,20 +473,201 @@ def edit_work_experience():
             print(f"No work experiences found at '{company_to_edit}'.")
 
 def analyze_job():
-    valid = {'1': create_resume, '2': create_cover_letter}
-    print("Would you like to create a resume or a cover letter?\n")
-    print("1. Create Resume")
-    print("2. Create Cover Letter\n")
+    valid = {'1': add_job, '2': create_resume, '3': create_cover_letter}
+    print("Would you like to input a new job, create a resume or a cover letter?\n")
+    print("1. Add Job")
+    print("2. Create Resume")
+    print("3. Create Cover Letter\n")
     while True:
         result = input("Selection: ")
         if result in valid:
             return valid[result]()
-        print("Invalid input. Please type 1 to create a resume, or 2 to create a cover letter")
+        print("Invalid input. Please type 1 to add a job, 2 to create a resume, or 3 to create a cover letter")
 
+def add_job():
+    valid = {'1': job_scraper, '2': custom_job_description, '3': main_menu}
+    print("Would you like to perform a job search across job boards, input a custom job description, or return to the main menu?\n")
+    print("1. Job Search")
+    print("2. Custom Job Description")
+    print("3. Return to Main Menu\n")
+    while True:
+        result = input("Selection: ")
+        if result in valid:
+            return valid[result]()
+        print("Invalid input. Please type 1 to perform a job search, 2 to input a custom job description, or 3 to return to the main menu")
+
+def job_scraper():
+    print("Please wait, initializing searcher...")
+    
+    autocomplete_model = ChatOllama(model="gemma3:1b")
+    
+    while True:
+        search_query = input("Enter the job title or keywords to search for: ")
+        location = input("Enter the location for the job search (or leave blank for remote): ")
+        num_results = input("Enter the number of job listings to retrieve: ")
+        distance = input("Enter the maximum distance from the location in miles (or leave blank for no limit): ")
+        job_type = input("Enter the job type (fulltime, parttime, internship, contract) (or leave blank for any): ")
+        remote = input("Should the job be remote? (y/n) (or leave blank for any): ")
+        
+        if not num_results.isdigit() or int(num_results) <= 0:
+            print("Invalid input. Please enter a positive integer for the number of job listings.")
+            continue
+        
+        if distance.strip() != "" and (not distance.isdigit() or int(distance) < 0):
+            print("Invalid input. Please enter a non-negative integer for the distance.")
+            continue
+        
+        if remote.lower() not in {'y', 'n', ''}:
+            print("Invalid input for remote. Please enter 'y' for yes, 'n' for no, or leave blank for any.")
+            continue
+        
+        
+        if job_type.strip() != "" and job_type.lower() not in {'fulltime', 'parttime', 'internship', 'contract'}:
+            print("Invalid input for job type. Please enter 'fulltime', 'parttime', 'internship', 'contract', or leave blank for any.")
+            continue
+        
+        print("\nSearching for jobs...\n")
+        
+        generate_google_search_prompt = """
+        Generate a concise Google search query to find job listings based on the following parameters. If the location field is blank, assume that the job is remote. Do not include anything else in your response but the query:
+                
+        Job Title/Keywords: {search_query}
+        Location: {location}
+        Distance from location (in miles, may be blank): {distance}
+        Job Type (fulltime, parttime, internship, contract, may be blank): {job_type}
+        """
+
+        formatted_prompt = generate_google_search_prompt.format(
+            search_query=search_query,
+            location=location,
+            distance=distance,
+            job_type=job_type
+        ).strip()
+
+        google_search_query = autocomplete_model.invoke(formatted_prompt)
+        
+        print(f"Generated Google Search Query: {google_search_query.content.strip()}\n")
+        
+        job_listings = scrape_jobs(
+            site_name=["indeed", "linkedin", "zip_recruiter", "google"],
+            search_query=search_query,
+            location=location,
+            results_wanted=int(num_results),
+            distance=int(distance) if distance.strip() != "" else None,
+            job_type=job_type.lower() if job_type.strip() != "" else None,
+            remote=True if remote.lower() == 'y' else False if remote.lower() == 'n' else None,
+            convert_to_annual=True,
+            verbose=1,
+            linkedin_fetch_description = True,
+            google_search_term=google_search_query.content.strip()
+        )
+        
+        job_result_df = pd.DataFrame(job_listings)
+        print("RESULTS:\n" + "-"*30)
+        print(job_result_df)
+        print("-"*30 + "\n")
+        
+        job_df = pd.read_csv('Stored Info/job_bank.csv')
+        job_df = pd.concat([job_df, job_result_df], ignore_index=True)
+        
+        job_df = job_df.drop_duplicates()
+        
+        path = Path("Stored Info/job_bank.csv")
+        job_df.to_csv(path, index=False)
+        print(f"Job search bank updated")
+        
+        print("\nWould you like to perform another job search? (y / anything else to return to job addition menu)")
+        again = input("Selection: ")
+        if again.lower() != 'y':
+            return add_job()
+
+def custom_job_description():
+    while True:
+        print("\n"+"-"*30)
+        print("You will be asked to input several fields. If you do not have a field, just leave it blank and press enter.")
+        print("-"*30+"\n")
+        
+        job_url = input("Enter the job URL: ")
+        title = input("Enter the job title: ")
+        company = input("Enter the company name: ")
+        location = input("Enter the job location: ")
+        description = input("Enter the job description: ")
+        skills = input("Enter the required skills (comma separated): ")
+        type_of_salary = input("Enter the type of salary (e.g., hourly, yearly): ")
+        min_salary = input("Enter the minimum salary amount: ")
+        max_salary = input("Enter the maximum salary amount: ")
+        type_of_job = input("Enter the type of job (e.g., fulltime, parttime, internship, contract): ")
+        
+        id = "custom"+datetime.now().strftime("%Y%m%d%H%M%S")
+        
+        new_job_entry = {
+            "id": id,
+            "site": None,
+            "job_url": job_url,
+            "job_url_direct": job_url,
+            "title": title,
+            "company": company,
+            "location": location,
+            "date_posted": date.today().isoformat(),
+            "job_type": type_of_job,
+            "salary_source": "user_input",
+            "interval": type_of_salary,
+            "min_amount": float(min_salary) if min_salary else None,
+            "max_amount": float(max_salary) if max_salary else None,
+            "currency": "USD",
+            "is_remote": None,
+            "job_level": None,
+            "job_function": None,
+            "listing_type": None,
+            "emails": None,
+            "description": description,
+            "company_industry": None,
+            "company_url": None,
+            "company_logo": None,
+            "company_url_direct": None,
+            "company_addresses": None,
+            "company_num_employees": None,
+            "company_revenue": None,
+            "company_description": None,
+            "skills": [s.strip() for s in skills.split(",")],
+            "experience_range": None,
+            "company_rating": None,
+            "company_reviews_count": None,
+            "vacancy_count": 1,
+            "work_from_home_type": None
+        }
+        
+        job_df = pd.read_csv('Stored Info/job_bank.csv')
+        job_df = pd.concat([job_df, pd.DataFrame([new_job_entry])], ignore_index=True)
+        
+        job_df = job_df.drop_duplicates()
+            
+        path = Path("Stored Info/job_bank.csv")
+        job_df.to_csv(path, index=False)
+        print(f"Job search bank updated")
+            
+        print("\nWould you like to input another job? (y / anything else to return to job addition menu)")
+        again = input("Selection: ")
+        if again.lower() != 'y':
+            return add_job()
+    
 def create_resume():
-    pass
+    generative_model = ChatOllama(model="deepseek-r1", streaming=True, reasoning=True)
+    
+    # General idea is to have a latex template for a resume, where each project, skill, course and work experience is a snippet
+    # that gets filled in based on the user's data and the job description. The AI will rank the user's data based on relevance to the job,
+    # and then say what skills, previous work experience, projects and coursework to include in the resume.
+    # A regular PY function will then take the AI generated ranking and construct the latex file, compile it to pdf, and save it to Outputs/Resumes.
+    
+    # maybe figure out how to implement genetic algorithms here, e.g. generate a few good resumes and then have the user pick the best one
+    # then mutate that one to make a few more options, repeat until satisfied. Or have the AI do it by prompting it to improve on the previous version.
 
 def create_cover_letter():
+    
+    # Similar to resume creation, but with a cover letter template. Don't think LaTeX is a good option here, but who knows. 
+    # Maybe figure out how to use google docs/word templates with LLM's.
+    # Maybe also figure out a way to use genetic algorithms here as well.
+    
     pass
 
 def init_check(): # check to make sure all necessary folders and files are made
@@ -495,7 +686,7 @@ def init_check(): # check to make sure all necessary folders and files are made
             "Resumes",
         ],
         "Stored Info": [
-            "Job Descriptions (Previously Applied)",
+            "job_bank.csv",
             "coursework_bank.csv",
             "projects_bank.csv",
             "skills_bank.csv",
@@ -508,6 +699,42 @@ def init_check(): # check to make sure all necessary folders and files are made
         "projects_bank.csv": ["project_name", "description", "start_date", "end_date", "link1", "link2"],
         "skills_bank.csv": ["skill_name", "level"],
         "work_experience_bank.csv": ["company", "role", "start_date", "end_date", "description"],
+        "job_bank.csv": [
+                "id",
+                "site",
+                "job_url",
+                "job_url_direct",
+                "title",
+                "company",
+                "location",
+                "date_posted",
+                "job_type",
+                "salary_source",
+                "interval",
+                "min_amount",
+                "max_amount",
+                "currency",
+                "is_remote",
+                "job_level",
+                "job_function",
+                "listing_type",
+                "emails",
+                "description",
+                "company_industry",
+                "company_url",
+                "company_logo",
+                "company_url_direct",
+                "company_addresses",
+                "company_num_employees",
+                "company_revenue",
+                "company_description",
+                "skills",
+                "experience_range",
+                "company_rating",
+                "company_reviews_count",
+                "vacancy_count",
+                "work_from_home_type"
+            ]
     }
 
     for folder, contents in structure.items():
